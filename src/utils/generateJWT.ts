dfb618bdc00473dd8e0e23fceeef5c122dd1bfd4
Moxie <ysmarina1234@gmail.com>
2024-02-09 21:03:21 +0530

    adding generateJWT description

--- src/utils/generateJWT.ts
+++ src/utils/generateJWT.ts
@@ -1,3 +1,10 @@
+/**
+ * This file is used to generate a JWT token using the jsonwebtoken library.
+ * The function generateJWT takes in a payload and an optional expiresIn parameter and returns a JWT token.
+ * The default value for expiresIn is 1 hour.
+ * Ensure that you have a .env file with a JWT_SECRET_KEY variable.
+ */
+
 import { sign } from 'jsonwebtoken';
 import { config } from 'dotenv';
 
@@ -7,11 +14,11 @@ export interface Payload {
 	[key: string]: any;
 }
 
-export const generateJWT = (payload: Payload): string => {
-	const secret = process.env.SECRET_KEY;
+export const generateJWT = (payload: Payload, expiresIn: string = '1h'): string => {
+	const secret = process.env.JWT_SECRET_KEY;
 	
 	if (!secret)
 		return 'Secret key is missing for generateJWT function. Please provide a SECRET_KEY in the .env file.';
 
-	return sign(payload, secret);
+	return sign(payload, secret, { expiresIn: expiresIn });
 };
